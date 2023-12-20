a3c4e62aeeab20dc3a48e3f639f9e93f186c4779
Kleyton Caires <kleytoncaires@gmail.com>
2023-12-20 15:54:34 -0300

    ajustes

---
+++ page-desenvolve-minas.php
@@ -0,0 +1,83 @@
+<?php get_header(); ?>
+
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Library</li>
+    </ol>
+</nav>
+
+<section class="py-50 py-lg-50">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10">
+                <h2 class="heading mb-15">O que é o Desenvolve Minas?</h2>
+
+                <div class="small-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, cupiditate cum? Ullam pariatur, in voluptatem illo voluptates quasi nostrum ratione, magni earum laborum rerum vero quidem, laboriosam debitis sint distinctio!
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section class="py-50 py-lg-50">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10">
+                <h2 class="sub-heading mb-25">O que é o Desenvolve Minas?</h2>
+
+                <div class="row">
+                    <div class="col-12 col-lg-3 text-center">
+                        <div class="bg-white py-40 shadow">
+                            <i class="icon-book color-red" style="font-size: 35px;"></i>
+                            <p class="text-uppercase small-text mt-15">Termos</p>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-lg-3 text-center">
+                        <div class="bg-white py-40 shadow">
+                            <i class="icon-book color-red" style="font-size: 35px;"></i>
+                            <p class="text-uppercase small-text mt-15">Termos</p>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-lg-3 text-center">
+                        <div class="bg-white py-40 shadow">
+                            <i class="icon-book color-red" style="font-size: 35px;"></i>
+                            <p class="text-uppercase small-text mt-15">Termos</p>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-lg-3 text-center">
+                        <div class="bg-white py-40 shadow">
+                            <i class="icon-book color-red" style="font-size: 35px;"></i>
+                            <p class="text-uppercase small-text mt-15">Termos</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="small-text mt-50 mt-lg-50">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, cupiditate cum? Ullam pariatur, in voluptatem illo voluptates quasi nostrum ratione, magni earum laborum rerum vero quidem, laboriosam debitis sint distinctio!
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="background-gray py-50 py-lg-50">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10">
+                <div class="ratio ratio-16x9 background-cover" style="background-image: url('https://picsum.photos/1280/720.jpg');"></div>
+
+                <div class="small-text mt-50 mt-lg-50">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, cupiditate cum? Ullam pariatur, in voluptatem illo voluptates quasi nostrum ratione, magni earum laborum rerum vero quidem, laboriosam debitis sint distinctio!
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
